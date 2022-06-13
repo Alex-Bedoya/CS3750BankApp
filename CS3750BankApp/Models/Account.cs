0dfa2ed3dac83b86b7d1e8cf5c2d4c934afa202d
Adam Aguirre <adamaguirre@weber.edu>
2022-06-13 17:48:07 -0600

    Adams page

---
+++ CS3750BankApp/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace CS3750BankApp.Models
+{
+    public class Account
+    {
+
+        public int ID { get; set; }
+        public string AccountNumber { get; set; }
+        public string Type { get; set; }
+        public int Balance { get; set; }
+
+        //DB will take care of this later.
+        public Account(int iD, string accountNumber, string type, int balance)
+        {
+            ID = iD;
+            AccountNumber = accountNumber;
+            Type = type;
+            Balance = balance;
+        }
+    }
+}

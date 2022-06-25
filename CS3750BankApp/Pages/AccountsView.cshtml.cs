704bbea100b40cdc5b16f92bbc2379bc54fdf3c6
Adam Aguirre <adamaguirre@weber.edu>
2022-06-25 14:30:17 -0600

    Fixed a merging error

--- CS3750BankApp/Pages/AccountsView.cshtml.cs
+++ CS3750BankApp/Pages/AccountsView.cshtml.cs
@@ -7,10 +7,11 @@ namespace CS3750BankApp.Pages
 {
     public class AccountsModel : PageModel
     {
+        public int accountNum { get; set; }
         public List<Account> Accounts { get; set; }
         public void OnGet()
         {
-            int accountNum = (int)HttpContext.Session.GetInt32("_Account");
+            accountNum = (int)HttpContext.Session.GetInt32("_Account");
 
             Accounts = DbRepository.GetSubAccounts(accountNum);
         }

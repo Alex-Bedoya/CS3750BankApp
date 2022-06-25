5ee909f5882a5b7d683793d540f7f93748ae1213
Adam Aguirre <adamaguirre@weber.edu>
2022-06-25 12:43:23 -0600

    Sign in redirect and checks

--- CS3750BankApp/Pages/AccountsView.cshtml.cs
+++ CS3750BankApp/Pages/AccountsView.cshtml.cs
@@ -1,3 +1,5 @@
+using CS3750BankApp.DataAccess;
+using CS3750BankApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,11 @@ namespace CS3750BankApp.Pages
 {
     public class AccountsModel : PageModel
     {
+        //public List<Account> Accounts { get; set; }
         public void OnGet()
         {
+            /*int accountNum; Int32.TryParse(HttpContext.Session.Get("Account").ToString(), out accountNum);
+            Accounts = DbRepository.GetSubAccounts(accountNum);*/
         }
     }
 }

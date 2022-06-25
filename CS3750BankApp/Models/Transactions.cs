62d2a62b500e83693b14cdf4c8e809d31aa4940c
Adam Aguirre <adamaguirre@weber.edu>
2022-06-25 17:29:18 -0600

    Withdraw, Deposit and Transfer

--- CS3750BankApp/Models/Transactions.cs
+++ CS3750BankApp/Models/Transactions.cs
@@ -7,10 +7,10 @@
         public int AccountNumber { get; set; }
 
         public string Date { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int Amount { get; set; }
-        public string Sender { get; set; }
-        public string Reciever { get; set; }
+        public string? Sender { get; set; }
+        public string? Reciever { get; set; }
         public string TransactionType { get; set; }
 
 
